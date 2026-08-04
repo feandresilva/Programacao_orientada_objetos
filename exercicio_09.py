c2409ac6c90d4589cc1015f02bd03e79c4ef1e3a
Felipe <felipebio97@gmail.com>
2026-08-04 14:25:08 -0300

    Criand sistema 1.2

--- exercicio_09.py
+++ exercicio_09.py
@@ -72,3 +72,15 @@ class Atendimento:
 
     def entrar_na_fila(self, paciente):
         self.numero_atendimento.append(self.fila_pacientes)
+        
+    
+    def chamar_proximo(self):
+    
+
+    def iniciar_coleta(self):
+
+
+    def _finalizar_atendimento(self):
+
+
+    def exibir_resumo(self):
