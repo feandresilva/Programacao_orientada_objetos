c8650465282e35f87b965de67b710c61b9a4b4a8
Felipe <felipebio97@gmail.com>
2026-08-05 19:05:12 -0300

    Criando sistema 1.10

--- exercicio_09.py
+++ exercicio_09.py
@@ -121,58 +121,92 @@ class Colesterol(Exame):
     def exibir_preparo(self):
         print("Beber água")
 
-novo_paciente = Paciente()
-hemograma_exame = Hemograma()
-glicemia_exame = Glicemia()
-colesterol_exame = Colesterol()
+def cadastrar_novo_usuario():
+    while True:
+        novo_paciente = Paciente()
+        novo_paciente.nome = input("Por favor, digite o nome do paciente: ").strip().capitalize()
+        try:
+            novo_paciente.cpf = int(input("Por favor, digite o CPF do paciente: "))
+        except ValueError:
+            print("Opção inválida! Por favor, tente novamente")
+            continue
+        novo_paciente.data_nascimento = input("Por favor, digite a data de nascimento do paciente: ")
+        try:
+            novo_paciente.numero_atendimento = input("Por favor, digite o número de atendimento do paciente: ")
+        except ValueError:
+            print("Opção inválida! Por favor, tente novamente")
+            continue
+        return novo_paciente
+
+lista_pacientes = []
+lista_de_exames = []
+
 while True:
     print("MENU")
     print("1. Cadastrar novo paciente")
-    print("2. Incluir exames ")
-    print("3. Adicionar atendimento na fila")
-    print("4. Visualizar fila")
-    print("5. Chamar próximo paciente")
-    print("6. Iniciar coleta")
-    print("7. Finalizar atendimento")
+    print("2. Adicionar atendimento na fila")
+    print("3. Visualizar fila")
+    print("4. Chamar próximo paciente")
+    print("5. Iniciar coleta")
+    print("6. Finalizar atendimento")
+    
     try:
         opcao_inicial = int(input("O que deseja fazer? Digite a opção escolhida: "))
     except ValueError:
         print("Opção inválida! Por favor, digite um número válido!")
         continue
     if opcao_inicial == 1:
-        novo_paciente.nome = input("Por favor, digite o nome do paciente: ").strip().capitalize()
-        try:
-            novo_paciente.cpf = int(input("Por favor, digite o CPF do paciente: "))
-        except ValueError:
-            print("Opção inválida! Por favor, tente novamnte")
-            continue
-        novo_paciente.data_nascimento = input("Por favor, digite a data de nascimento do paciente: ")
-        try:
-            novo_paciente.numero_atendimento = input("Por favor, digite o número de atendimento do paciente: ")
-        except ValueError:
-            print("Opção inválida! Por favor, tente novamnte")
-            continue
+        novo_paciente = cadastrar_novo_usuario()
+        lista_pacientes.append(novo_paciente)
         novo_paciente.exibir_dados()
     elif opcao_inicial == 2:
-        definicao_exame = input("Qual exame deseja fazer? Hemograma, Glicemia ou Colesterol? ").strip().capitalize()
-        if definicao_exame == "Hemograma":
-            hemograma_exame.codigo = input("Digite aqui o código do exame: ")
-            hemograma_exame.nome = input("Digite aqui o nome do exame: ")
-            hemograma_exame.preco = float(input("Digite aqui o preço (R$) do exame: "))
-            hemograma_exame.necessita_jejum = input("Digite aqui se há necessidade de jejum: ").strip().capitalize()
-            hemograma_exame.exibir_dados()
-        elif definicao_exame == "Glicemia":
-            glicemia_exame.codigo = input("Digite aqui o código do exame: ")
-            glicemia_exame.nome = input("Digite aqui o nome do exame: ")
-            glicemia_exame.preco = float(input("Digite aqui o preço (R$) do exame: "))
-            glicemia_exame.necessita_jejum = input("Digite aqui se há necessidade de jejum: ").strip().capitalize()
-            glicemia_exame.exibir_dados()
-        elif definicao_exame == "Colesterol":
-            colesterol_exame.codigo = input("Digite aqui o código do exame: ")
-            colesterol_exame.nome = input("Digite aqui o nome do exame: ")
-            colesterol_exame.preco = float(input("Digite aqui o preço (R$) do exame: "))
-            colesterol_exame.necessita_jejum = input("Digite aqui se há necessidade de jejum: ").strip().capitalize()
-            colesterol_exame.exibir_dados()
-        else:
-            print("Opção inválida! Tente novamente")
-            continue
+        novo_atendimento = Atendimento()
+        while True:
+            print("1. Adicionar paciente")
+            print("2. Adicionar exame")
+            print("3. Concluir")
+            try:
+                decisao_menu_interno = int(input("Digite aa opção desejada: "))
+            except ValueError:
+                print("Erro! Por favor, digite um número válido")
+                continue
+            if decisao_menu_interno == 1:
+                checagem_cadastro_usuario = input("O usuário já está cadastrado? Digite Sim ou Não ").strip().capitalize()
+                if checagem_cadastro_usuario == "Sim":
+                    print(f'Lista de pacientes cadastrados: {lista_pacientes}')
+                    paciente_selecionado = int(input("Selecione o índice do paciente desejado: "))
+                    novo_atendimento.paciente = lista_pacientes[paciente_selecionado]
+                elif checagem_cadastro_usuario == "Não":
+                    cadastrar_novo_usuario()
+                    lista_pacientes.append(novo_paciente)
+                    novo_paciente.exibir_dados()
+    #     hemograma_exame = Hemograma()
+    #     definicao_exame = input("Qual exame deseja fazer? Hemograma, Glicemia ou Colesterol? ").strip().capitalize()
+    #     if definicao_exame == "Hemograma":
+    #         hemograma_exame.codigo = input("Digite aqui o código do exame: ")
+    #         hemograma_exame.nome = input("Digite aqui o nome do exame: ")
+    #         hemograma_exame.preco = float(input("Digite aqui o preço (R$) do exame: "))
+    #         hemograma_exame.necessita_jejum = input("Digite aqui se há necessidade de jejum: ").strip().capitalize()
+    #         lista_de_exames.append(hemograma_exame)
+    #         hemograma_exame.exibir_dados()
+    #     elif definicao_exame == "Glicemia":
+    #         glicemia_exame = Glicemia()
+    #         glicemia_exame.codigo = input("Digite aqui o código do exame: ")
+    #         glicemia_exame.nome = input("Digite aqui o nome do exame: ")
+    #         glicemia_exame.preco = float(input("Digite aqui o preço (R$) do exame: "))
+    #         glicemia_exame.necessita_jejum = input("Digite aqui se há necessidade de jejum: ").strip().capitalize()
+    #         lista_de_exames.append(glicemia_exame)
+    #         glicemia_exame.exibir_dados()
+    #     elif definicao_exame == "Colesterol":
+    #         colesterol_exame = Colesterol()
+    #         colesterol_exame.codigo = input("Digite aqui o código do exame: ")
+    #         colesterol_exame.nome = input("Digite aqui o nome do exame: ")
+    #         colesterol_exame.preco = float(input("Digite aqui o preço (R$) do exame: "))
+    #         colesterol_exame.necessita_jejum = input("Digite aqui se há necessidade de jejum: ").strip().capitalize()
+    #         lista_de_exames.append(colesterol_exame)
+    #         colesterol_exame.exibir_dados()
+    #     else:
+    #         print("Opção inválida! Tente novamente")
+    #         continue
+    # elif opcao_inicial == 3:
+
